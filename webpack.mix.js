6605f709c6d693fd7662ee598b8ce77e095a88e5
AliRazavi1998 <razavi.ali.1998@gmail.com>
2021-02-25 23:28:58 +0330

    add front

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,23 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy("resources/assets/fonts","public/fonts")
+    .styles([
+        "resources/assets/css/font-awesome.min.css",
+        "resources/assets/css/materialdesignicons.css",
+        "resources/assets/css/materialdesignicons.css.map",
+        "resources/assets/css/bootstrap.css",
+        "resources/assets/css/owl.carousel.min.css",
+        "resources/assets/css/responsive.css",
+        "resources/assets/css/main.css",
+
+    ],"public/css/main.css")
+
+    .scripts([
+        "resources/assets/js/jquery-3.2.1.min.js",
+        "resources/assets/js/bootstrap.js",
+        "resources/assets/js/owl.carousel.min.js",
+        "resources/assets/js/main.js",
+    ],"public/js/main.js")
+
