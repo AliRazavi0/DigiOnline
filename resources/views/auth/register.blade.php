b34e4f875a0de4718195d6fff328a6abf9142c9f
AliRazavi1998 <razavi.ali.1998@gmail.com>
2021-02-25 23:49:31 +0330

    add front login register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,89 @@
 @extends('layouts.app')
 
+
+@section('title')
+    ثبت نام در وب سایت
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+<section class="page-account-box">
+    <div class="col-lg-7 col-md-7 col-xs-12 mx-auto">
+        <div class="account-box">
+            <a href="#" class="account-box-logo">
+                {{ config('app.name') }}
+            </a>
+            <div class="account-box-headline">
+                <a href="{{ route('login') }}" class="login-ds ">ورود</a>
+                <a href="{{ route('register') }}" class="register-ds active-account">ثبت نام</a>
+            </div>
+            <div class="account-box-content">
+                <form action="{{ route('register') }}" class="form-account" method="POST">
+                    <div class="form-account-title">
+                        <label for="name">نام کاربری </label>
+                        <input class="number-email-input @error('name') is-invalid @enderror" id="name" type="name"
+                            name="name" value="{{ old('name') }}" required autocomplete="name" autofocus
+                            placeholder="نام کاربری خود را وارد کنید ...">
+                        <span class="mdi mdi-account-outline"></span>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-account-title">
+                        <label for="email">ایمیل </label>
+                        <input class="number-email-input @error('email') is-invalid @enderror" id="email" type="email"
+                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
+                            placeholder="ایمیل خود را وارد کنید ...">
+                        <span class="mdi mdi-account-outline"></span>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-account-title">
+                        <label for="password">رمز عبور</label>
+                        <input type="password" class="password-input @error('password') is-invalid @enderror"
+                            name="password" required autocomplete="current-password"
+                            placeholder="رمز عبور خود را وارد کنید ...">
+                        <span class="mdi mdi-lock"></span>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-account-title">
+                        <label for="password_confirmation">تکرار رمز  عبور</label>
+                        <input type="password" class="password-input @error('password') is-invalid @enderror"
+                            name="password_confirmation" required autocomplete="current-password"
+                            placeholder="تکرار رمز عبور خود را وارد کنید ...">
+                        <span class="mdi mdi-lock"></span>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                    </div>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    <div class="parent-btn lr-ds">
+                        <button class="dk-btn dk-btn-info">
+                            ثبت نام در {{ config('app.name') }}
+                            <i class="fa fa-sign-in sign-in"></i>
+                        </button>
+                    </div>
+                    <div class="forget-password">
+                        <a href="{{ route('password.request') }}" class="account-link-password">رمز خود را فراموش کرده
+                            ام</a>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
+</section>
+
 @endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer-light">
+    <div class="container">
+        <ul class="footer-light-link">
+            <li><a href="#">درباره {{ config('app.name') }}</a></li>
+            <li><a href="#">فرصت‌های شغلی</a></li>
+            <li><a href="#">تماس با ما</a></li>
+            <li><a href="#">همکاری با سازمان‌ها</a></li>
+        </ul>
+
+        <p class="title-footer">
+            حقوق این وب سایت متعلق به {{ config('app.name') }}
+        </p>
+
+        <p class="copy-right-footer-light">Copyright © 2020 - 2021 {{ config('app.url') }}</p>
+    </div>
+</footer>
